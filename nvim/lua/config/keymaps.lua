021c229fd27c44359c3176c3dd285b9c29cb6a9a
ultimatile <ultimatile@users.noreply.github.com>
2024-06-19 21:13:42 +0900

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,16 +3,24 @@
 -- Add any additional keymaps here
 
 local function remap_keys(lhs, rhs)
-    vim.api.nvim_set_keymap('n', lhs, rhs, { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
 end
 
-remap_keys('<CR><CR>', '<C-w><C-w>')
-remap_keys('j', 'h') -- go left
-remap_keys('k', 'gk') -- go up
-remap_keys('l', 'gj') -- go down
-remap_keys(';', 'l') -- go right
-remap_keys('h', ';')
+-- escaping by double return
+remap_keys("<CR><CR>", "<C-w><C-w>")
 
-vim.keymap.set({'i', 'v', 's'}, 'jj', '<Esc>', { noremap = true, silent = true })
+-- moving
+remap_keys("j", "h") -- go left
+remap_keys("k", "gk") -- go up
+remap_keys("l", "gj") -- go down
+remap_keys(";", "l") -- go right
+remap_keys("h", ";")
 
+--
+remap_keys("x", '"_x') -- character erasure without yank
+remap_keys("d", '"_d') -- opping yank when end-of-line erasure
+remap_keys("D", '"_D') -- topping yank when lines erasure
+
+-- escaping by jj
+vim.keymap.set({ "i", "v", "s" }, "", "<Esc>", { noremap = true, silent = true })
 --  vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
