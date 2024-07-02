aa5bc54361aab08cb2e2334b5b7f0fb6accbd30e
ultimatile <ultimatile@users.noreply.github.com>
2024-07-02 18:45:25 +0900

    update

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+{
+    -- "zbirenbaum/copilot.lua",
+    -- -- opts = {
+    --   suggestion = {
+    --     enabled = true,
+    --     keymap = { accept = "<Space>" },
+    --   },
+    -- -- },
+  },
+}
