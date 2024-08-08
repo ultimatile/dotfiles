1fd7b193b3377edf758dadebf75dfee55ff9c17d
ultimatile <ultimatile@users.noreply.github.com>
2024-08-08 23:48:05 +0900

    update options location

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- stop relative line numbers
-vim.opt.relativenumber = false
-
--- Disable conceal
-vim.opt.conceallevel = 0
-
--- enable line wrapping
-vim.o.wrap = true
-
--- enable yank to clipboard
-vim.opt.clipboard = "unnamedplus"
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-return {
 
-}
+-- stop relative line numbers
+vim.opt.relativenumber = false
+
+-- Disable conceal
+vim.opt.conceallevel = 0
+
+-- enable line wrapping
+vim.o.wrap = true
+
+-- enable yank to clipboard
+-- vim.opt.clipboard = "unnamedplus"
+
