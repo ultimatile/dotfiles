03e69907edfe162ed31cf287e3982a22ec53be02
ultimatile <ultimatile@users.noreply.github.com>
2024-08-15 06:55:55 +0900

    refactor for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    # Homebrew
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-
-    # Rye
-    set -Ua fish_user_paths "$HOME/.rye/shims"
-
-    # Python
-    source ~/RyePython/.venv/bin/activate.fish
-
     # alias
     alias g="git"
     alias gpomn="git push origin main"
@@ -17,6 +7,7 @@ if status is-interactive
     alias gcltt="git clone ultimatile:ultimatile/LaTeXTikZTemplate.git"
     alias gp="gnuplot"
     alias nv="nvim"
+    alias view="nvim -R"
     alias brup="brew upgrade"
     alias pbpc="pbpaste | sed -e 's/\([.?!]\) /\1\n/g' -e 's/-\ //g'|pbcopy"
     alias sed="gsed"
@@ -31,15 +22,25 @@ if status is-interactive
     alias sl="ls"
     alias ks="ls"
     alias l="ls"
+    alias ll="eza"
+
+    alias treee="eza -T"
 
     # Rosetta terminal
     alias zarm="env /usr/bin/arch -arm64 /bin/zsh --login"
     alias zintel="env /usr/bin/arch -x86_64 /bin/zsh --login"
 end
 
+# Rye
+set -Ua fish_user_paths "$HOME/.rye/shims"
+# Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+# Python
+source ~/RyePython/.venv/bin/activate.fish
 source /opt/homebrew/opt/modules/init/fish
 module use /Users/$USER/modulefiles
 set icloud "/Users/$USER/Library/Mobile Documents/com~apple~CloudDocs"
+set AQC "/Users/$USER/Library/Mobile Documents/com~apple~CloudDocs/AQC"
 source "$HOME/.cargo/env.fish"
 
 #HOGE=fugaとしていました。これがset HOGE fugaに変わります。
