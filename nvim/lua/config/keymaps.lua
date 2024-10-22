166768672c1465c88820cce230aea15b0cdf6f73
ultimatile <ultimatile@users.noreply.github.com>
2024-10-22 17:47:16 +0900

    nvim: add new keymap to move the current line up/down

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,3 +27,28 @@ remap_keys("<leader>d", "dd")
 -- escaping by jj
 vim.keymap.set({ "i", "v", "s" }, "jj", "<Esc>", { noremap = true, silent = true })
 --  vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+-- show all the hidden diagnostics
+vim.keymap.set("n", "<leader>m", vim.diagnostic.open_float, { noremap = true, silent = true })
+
+-- Normal mode mappings
+-- move the line up
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-S-Up>",
+  '":move -1-" .. v:count1 .. "<CR>==l"',
+  { noremap = true, expr = true, silent = true }
+)
+-- move the line down
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-S-Down>",
+  '":move +1<CR>==" .. v:count1 .. "l"',
+  { noremap = true, expr = true, silent = true }
+)
+
+-- Visual mode mappings
+-- move the line up
+vim.api.nvim_set_keymap("x", "<C-S-Up>", ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
+-- move the line down
+vim.api.nvim_set_keymap("x", "<C-S-Down>", ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
