5bb48a23bd9e1bd8e6c4c3b34917ab1437df7273
ultimatile <ultimatile@users.noreply.github.com>
2024-10-28 00:52:44 +0900

    nvim: add autocmd to leave Japanese IME

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set up the autocommand for both events
+vim.api.nvim_create_autocmd({ "InsertLeave", "FocusGained" }, {
+  callback = function()
+    vim.fn.system("im-select com.apple.inputmethod.Kotoeri.RomajiTyping.Roman")
+  end,
+})
