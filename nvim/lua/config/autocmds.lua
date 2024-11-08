91943a4ea46c64f22189deb1c24c9ebd16b53511
ultimatile <ultimatile@users.noreply.github.com>
2024-11-08 15:33:53 +0900

    nvim: stop comment continuation

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -8,3 +8,17 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "FocusGained" }, {
     vim.fn.system("im-select com.apple.inputmethod.Kotoeri.RomajiTyping.Roman")
   end,
 })
+
+-- stop comment continuation
+-- https://github.com/LazyVim/LazyVim/discussions/598
+local augroup = vim.api.nvim_create_augroup
+local autocmds = vim.api.nvim_create_autocmd
+augroup("discontinue_comments", { clear = true })
+autocmds({ "FileType" }, {
+  pattern = { "*" },
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "o", "r" })
+  end,
+  group = "discontinue_comments",
+  desc = "no comment continuation",
+})
