979dffbc4c6acbb58859c5d642575324d96f05af
ultimatile <ultimatile@users.noreply.github.com>
2024-11-28 00:25:22 +0900

    nvim: formatting

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -7,8 +7,8 @@ return {
         enabled = true,
         auto_trigger = true,
         keymap = {
-          accept = "<C-v>",
-          accept_word = "<C-S-v>",
+          accept = "<C-V>",
+          accept_word = "<C-S-V>",
           -- If you want to use <Tab> to accept, you have to configure nvim-cmp's supertab setting
           -- https://www.lazyvim.org/configuration/recipes#supertab
           --accept = "<Tab>",
