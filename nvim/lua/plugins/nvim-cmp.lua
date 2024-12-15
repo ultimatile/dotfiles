933b52a9df372a9a307d4b3e9992b058935b3502
ultimatile <ultimatile@users.noreply.github.com>
2024-12-15 10:07:18 +0900

    nvim: refactoring for nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,35 +1,10 @@
 return {
   "hrsh7th/nvim-cmp",
-  ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local cmp = require("cmp")
-    local copilot = require("copilot.suggestion")
-    local accept_WORD = function()
-      copilot.accept(function(suggestion)
-        local range, text = suggestion.range, suggestion.text
-
-        local cursor = vim.api.nvim_win_get_cursor(0)
-        local _, character = cursor[1], cursor[2]
-        local _, char_idx = string.find(text, "[^%s]+", character + 1)
-        if char_idx then
-          suggestion.text = string.sub(text, 1, char_idx)
-
-          range["end"].line = range["start"].line
-          range["end"].character = char_idx
-        end
-        return suggestion
-      end)
-    end
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if copilot.is_visible() then
-          LazyVim.create_undo()
-          accept_WORD()
-          -- copilot.accept_word()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+      ["<S-Tab>"] = function(fallback)
+        return LazyVim.cmp.map({ "snippet_backward", "ai_accept_WORD" }, fallback)()
+      end,
     })
   end,
 }
