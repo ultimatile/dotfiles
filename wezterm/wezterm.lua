567f7e067a38c00c8f6e4570bc61fc30a7d99b9c
ultimatile <ultimatile@users.noreply.github.com>
2024-12-16 06:28:42 +0900

    [WIP]wezterm: tab and status bars config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,4 +30,40 @@ config.keys = {
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
+config.hide_tab_bar_if_only_one_tab = false
+config.window_decorations = "NONE"
+
+wezterm.on("update-status", function(window, pane)
+	local date = wezterm.strftime(" %m/%-d %H:%M ")
+
+	window:set_right_status(wezterm.format({
+		{ Text = wezterm.nerdfonts.fa_clock_o .. " " .. date },
+	}))
+	window:set_left_status(wezterm.format({
+		{ Text = wezterm.nerdfonts.fa_clock_o .. " " .. date },
+	}))
+end)
+
+config.tab_bar_at_bottom = true
+config.show_tabs_in_tab_bar = false
+wezterm.on("update-right-status", function(window, pane)
+	window:set_left_status("left")
+	window:set_right_status("right")
+end)
+
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+-- TODO: separate status bar and tab bar
+-- bottom status bar cannot be native, and it would be implemented by window_decorations
+
+-- TODO: how to add status icons (iTerm2's icons are image files, but nerdfonts would be enough)
+
+-- TODO: consider how to get cpu usage, memory usage, network io usage (fastfetch?)
+-- TODO: consider how to implement sparkline to visualize cpu usage, memory usage, network io usage
+
+-- TODO: get current git branch name
+
+-- TODO: get current directory name
 return config
