d34872ab8bcfe51b3504d6d91932489b41eda040
ultimatile <ultimatile@users.noreply.github.com>
2024-12-22 13:07:24 +0900

    nvim: remove augroup prefix

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,7 +3,7 @@
 -- Add any additional autocmds here
 -- local augroup = vim.api.nvim_create_augroup
 local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup(name, { clear = true })
 end
 local autocmds = vim.api.nvim_create_autocmd
 
