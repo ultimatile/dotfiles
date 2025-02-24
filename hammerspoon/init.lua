112b8735f668f6c0e9470e254e40a72d619bc0ea
ultimatile <ultimatile@users.noreply.github.com>
2025-02-24 19:36:08 +0900

    hammerspoon: add new keymap

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,14 @@
--- Function to execute the "swpunc" command
-
--- Bind Cmd+Shift+Esc to the runSwpunc function
+-- Bind Cmd+Ctrl+Esc to the runSwpunc function
 hs.hotkey.bind({ "cmd", "ctrl" }, "escape", function()
 	local output, ret = hs.execute("swpunc", true)
 	if ret then
 		hs.alert.show(output)
 	end
+end)
+-- Bind Cmd+Ctrl+c to the copy-flattern-split function
+hs.hotkey.bind({"cmd", "ctrl"}, "c", function()
+	hs.eventtap.keyStroke({"cmd"}, "c")
+	hs.timer.doAfter(0.3, function()
+		hs.execute("pbpaste | tr '\\n' ' ' | sed -e 's/\\([.?!]\\) /\\1\\n/g' -e 's/- //g' | pbcopy", true)
+	end)
 end)
