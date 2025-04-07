9f0b86b1895472dbb4f6cafc30b31e327e48c074
ultimatile <ultimatile@users.noreply.github.com>
2025-04-08 07:13:34 +0900

    nvim: temporal fix copilot-related error

---
+++ nvim/lua/plugins/fix-lualine-copilot.lua
@@ -0,0 +1,13 @@
+-- https://github.com/LazyVim/LazyVim/pull/5900#issuecomment-2781528747
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    opts.sections.lualine_x[2] = LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+      local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+      if #clients > 0 then
+        local status = require("copilot.status").data.status
+        return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+      end
+    end)
+  end,
+}
