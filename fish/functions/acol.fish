a7d0617483a11ae87ee0133ae5857800a436b0f4
ultimatile <ultimatile@users.noreply.github.com>
2025-05-22 17:38:10 +0900

    fish: update acol to accept multiple columns

--- fish/functions/acol.fish
+++ fish/functions/acol.fish
@@ -1,9 +1,14 @@
 function acol
     if test (count $argv) -eq 0
-        echo "Usage: f COLUMN_NUMBER"
+        echo "Usage: acol COLUMN_NUMBER [COLUMN_NUMBER...]"
         return 1
     end
-    set col $argv[1]
-    set prog (string join "" "{print \$" $col "}")
+    
+    set print_cols
+    for col in $argv
+        set -a print_cols "\$$col"
+    end
+    
+    set prog (string join "" "{print " (string join "," $print_cols) "}")
     awk $prog
 end
