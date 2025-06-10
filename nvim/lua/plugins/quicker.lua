41f2d96935a77f5e69719da4dc4ffb96580fa6e1
ultimatile <ultimatile@users.noreply.github.com>
2025-06-11 00:48:56 +0900

    nvim: add quickfix-local quicker's keymaps

--- nvim/lua/plugins/quicker.lua
+++ nvim/lua/plugins/quicker.lua
@@ -3,7 +3,24 @@ return {
   event = "FileType qf",
   ---@module "quicker"
   ---@type quicker.SetupOptions
-  opts = {},
+  opts = {
+    keys = {
+      {
+        ">",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand quickfix context",
+      },
+      {
+        "<",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context",
+      },
+    },
+  },
   keys = {
     {
       "<leader>9",
