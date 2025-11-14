846991e94b5f5b3916a23bcd79ad17542e863e03
ultimatile <ultimatile@users.noreply.github.com>
2025-11-14 19:10:59 +0900

    nvim: configure path completion from root to cwd

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -2,6 +2,14 @@ return {
   "saghen/blink.cmp",
   dependencies = { "kdheepak/cmp-latex-symbols", "saghen/blink.compat" },
   opts = {
+    -- fuzzy = {
+    --   max_typos = 0, -- Strict matching without typo tolerance
+    -- },
+    completion = {
+      keyword = {
+        range = "full", -- Consider full keyword context for matching
+      },
+    },
     keymap = {
       ["<S-Tab>"] = {
         LazyVim.cmp.map({ "snippet_backward", "ai_accept_WORD" }),
@@ -13,6 +21,19 @@ return {
       },
     },
     sources = {
+      providers = {
+        path = {
+          min_keyword_length = 2, -- Require at least 3 characters before triggering path completion
+          opts = {
+            trailing_slash = true,
+            label_trailing_slash = true,
+            get_cwd = function(_)
+              return vim.fn.getcwd()
+            end,
+            show_hidden_files_by_default = false,
+          },
+        },
+      },
       compat = { "latex_symbols" },
       per_filetype = {
         julia = { "lsp", "path", "snippets", "buffer", "latex_symbols" },
