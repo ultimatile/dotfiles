a531f6a52dde192972426737a70225f1e7091fac
ultimatile <ultimatile@users.noreply.github.com>
2025-12-15 00:23:13 +0900

    nvim: use github registry for jetls

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,20 +2,13 @@
 return {
   "mason-org/mason.nvim",
   opts = function(_, opts)
-    -- Add custom registry path to Lua package path
-    local registry_path = vim.fn.expand("~/ghq/github.com/ultimatile/mason-jetls-registry")
-    if vim.fn.isdirectory(registry_path) == 1 then
-      package.path = package.path .. ";" .. registry_path .. "/lua/?.lua"
-      package.path = package.path .. ";" .. registry_path .. "/lua/?/init.lua"
-    end
-
     -- Initialize registries table with default if it doesn't exist
     opts.registries = opts.registries or {
       "github:mason-org/mason-registry",
     }
 
-    -- Add custom registry at the beginning (higher priority)
-    table.insert(opts.registries, 1, "lua:mason-jetls-registry")
+    -- Add custom registry from GitHub at the beginning (higher priority)
+    table.insert(opts.registries, 1, "github:ultimatile/mason-jetls-registry")
 
     return opts
   end,
