215f4678c1985ae55e8fd25db03c6270a74dd7e6
ultimatile <ultimatile@users.noreply.github.com>
2025-12-18 00:00:07 +0900

    nvim: update lspconfig of jetls compatible with nvim 0.11

--- nvim/lua/plugins/jetls.lua
+++ nvim/lua/plugins/jetls.lua
@@ -1,39 +1,43 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ft = "julia",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local configs = require("lspconfig.configs")
-      local util = require("lspconfig.util")
+    opts = function(_, opts)
+      -- Define JETLS server configuration
+      opts.servers = opts.servers or {}
+      opts.servers.jetls = {
+        cmd = { "jetls" },
+        filetypes = { "julia" },
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          local root = util.root_pattern("Project.toml", "JuliaProject.toml", ".git")(fname)
+          return root or vim.fn.fnamemodify(fname, ":p:h")
+        end,
+        single_file_support = true,
+        settings = {},
+      }
 
-      -- Define JETLS if not already defined
-      if not configs.jetls then
-        configs.jetls = {
-          default_config = {
-            cmd = { "jetls" },
-            filetypes = { "julia" },
-            root_dir = function(fname)
-              local root = util.root_pattern("Project.toml", "JuliaProject.toml", ".git")(fname)
-              return root or vim.fn.fnamemodify(fname, ":p:h")
-            end,
-            single_file_support = true,
-            settings = {},
-          },
-        }
+      -- Setup function to register JETLS with lspconfig before LazyVim sets it up
+      opts.setup = opts.setup or {}
+      opts.setup.jetls = function(_, server_opts)
+        -- Register JETLS config with lspconfig if not already defined
+        local configs = require("lspconfig.configs")
+        if not configs.jetls then
+          configs.jetls = {
+            default_config = {
+              cmd = server_opts.cmd,
+              filetypes = server_opts.filetypes,
+              root_dir = server_opts.root_dir,
+              single_file_support = server_opts.single_file_support,
+              settings = server_opts.settings,
+            },
+          }
+        end
+        -- Return false to let LazyVim handle the setup with vim.lsp.config() and vim.lsp.enable()
+        return false
       end
 
-      -- Setup JETLS
-      lspconfig.jetls.setup({})
-
-      -- Configure diagnostics for virtual text (like LazyVim)
-      vim.diagnostic.config({
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "●",
-        },
-      })
+      return opts
     end,
   },
 }
+
