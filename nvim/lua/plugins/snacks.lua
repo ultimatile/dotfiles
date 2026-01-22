f7993306e9a8832c849ebeef18948d7ae10093a3
ultimatile <ultimatile@users.noreply.github.com>
2026-01-23 08:38:28 +0900

    nvim: ignore tex intermediates in explorer

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,6 +3,23 @@ return {
   opts = function(_, opts)
     table.insert(opts.dashboard.preset.keys, 9, { icon = "󱌣 ", key = "m", desc = "Mason", action = ":Mason" })
     opts.lazygit = { win = { keys = { term_normal = false } } }
+    opts.picker = opts.picker or {}
+    opts.picker.sources = opts.picker.sources or {}
+    opts.picker.sources.explorer = opts.picker.sources.explorer or {}
+    opts.picker.sources.explorer.exclude = {
+      "*.aux",
+      "*.out",
+      "*.toc",
+      "*.fls",
+      "*.fdb_latexmk",
+      "*.synctex.gz",
+      "*.bbl",
+      "*.blg",
+      "*.nav",
+      "*.snm",
+      "*.vrb",
+      "*.xdv",
+    }
     return opts
   end,
 }
