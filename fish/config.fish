af49ad9ad8bf041849bb3df7aa96e236d36ea5d1
ultimatile <ultimatile@users.noreply.github.com>
2026-02-18 08:17:29 +0900

    fish: update abbr

--- fish/config.fish
+++ fish/config.fish
@@ -147,7 +147,7 @@ if status is-interactive
 
     abbr -a cl claude
     abbr -a clhi "claude -p --model \"haiku\" \"hi\""
-    abbr -a kc kiro-cli chat --model claude-opus-4.5
+    abbr -a kc kiro-cli chat --model claude-opus-4.6
 
     abbr -a ssmb ssh mbpr
 
